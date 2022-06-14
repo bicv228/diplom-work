4dfd9269f895999fef93e4995d593b024cb40608
bicv228 <107227496+bicv228@users.noreply.github.com>
2022-06-14 16:50:45 +0300

    Update App.js

--- App.js
+++ App.js
@@ -2,20 +2,29 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import AboutScreen from './screens/AboutScreen'
+import SearchScreen from "./screens/SearchScreen";
 import MainScreen from "./screens/MainScreen";
-
-
+import SenderScreen from "./screens/SenderScreen";
 
 const Stack = createStackNavigator();
 export default class App extends React.Component{
   render() {
       return (
           <NavigationContainer>
-              <Stack.Navigator initialRouteName="Search">
+              <Stack.Navigator initialRouteName='Main'>
                   <Stack.Screen
-                      name="Lexical minimum app"
+                      name="Main"
                       component={MainScreen}
-                      option={{title: 'Search'}}
+                      options={{title:'lexical minimum'}}
+                  />
+                  <Stack.Screen
+                      name="Send"
+                      component={SenderScreen}
+                  />
+                  <Stack.Screen
+                      name="Search"
+                      component={SearchScreen}
+                      option={({route}) =>({title: route.title.params.name})}
                   />
                   <Stack.Screen
                       name="About"
