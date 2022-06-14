cfff3dd7d6ee6fc30adafc82f0b71fb4beae3587
bicv228 <107227496+bicv228@users.noreply.github.com>
2022-06-14 16:51:25 +0300

    Update MainScreen.js

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,88 +1,25 @@
 import React from 'react';
-import {StyleSheet,ScrollView, TextInput, Text, FlatList} from "react-native-web";
-import firebase from '../src/config'
-import {TouchableOpacity} from "react-native";
+import {StyleSheet,ScrollView,Button} from "react-native";
 export default class extends React.Component{
     constructor(props) {
         super(props);
-        this.state = {
-            list: [],
-            loading: true
-        };
+        this.state = {};
     }
-    getList = () =>
-    {
-        firebase.database().ref('/1YQG7H2FTltWuQoEl_wXnhCHF0LCShGUrhpbtEtgF-qc/LexicalMin').on('value', snapshot =>{
-            let terList = [];
-            snapshot.forEach((child) =>{
-                let obj ={
-                    key: child.key,
-                    termin: child.val().termin,
-                    vocabulary: child.val().vocabulary
-                }
-                terList.push(obj);
-            })
-            this.setState({
-                list: terList,
-                loading : false
-            })
-            console.log(terList, this.state.list);
-        })
-    }
-    componentDidMount(){
-        this.getList();
-    }
-    changeText= (text) =>{
-        if(text.length >= 3) {
-            let tempList = [];
-            this.state.list.forEach((item) =>{
-            if(item.termin.search(text) != -1)
-            {
-                tempList.push((item));
-            }
-            })
-            this.setState({
-                list:tempList
-            })
-        }
-        else{
-            this.getList();
-        }
-    }
+
     render() {
         return(
             <ScrollView style = {styles.container}>
-                <TextInput
-                    style = {styles.search}
-                    placeholder ="Search"
-                    onChangeText ={(text) => this.changeText(text)}
+                <Button
+                    title = 'Search'
+                    onPress={() => {
+                        this.props.navigation.navigate('Search');
+                    }}
                 />
-                <FlatList style= {styles.container}
-                          data={this.state.list}
-                          keyExtractor={(item)=>item.key}
-                          renderItem={({item})=>{
-                              if(item.termin == "")
-                              {
-                                  return(
-                                      <TouchableOpacity
-                                          style = {styles.container}
-                                          onPress={() => {
-                                              this.props.navigation.navigate('About',{name:item.termin, key: item.key});
-                                          }}>
-                                          <Text>Here is termin but we dont have termin name</Text>
-                                      </TouchableOpacity>
-                                  )
-                              }
-                              else return(
-                                  <TouchableOpacity
-                                      style = {styles.container}
-                                      onPress={() => {
-                                          this.props.navigation.navigate('About',{name:item.termin, key: item.key});
-                                      }}>
-                                      <Text>{item.termin}</Text>
-                                  </TouchableOpacity>
-                              )
-                          }}/>
+                <Button
+                    title = 'Send'
+                    onPress={() => {
+                        this.props.navigation.navigate('Send');
+                    }}/>
             </ScrollView>
         );
     }
